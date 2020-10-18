3be7441110233ab1649ced3a7919ae09877dc3c7
hayato-nima <kdc98bac72xkx@gmail.com>
2020-10-18 22:07:55 +0900

    初回コミット

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,17 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+  for ($j = 0; $j < ($count - 1 - $i); $j++) {
+    if ($array[$j + 1] < $array[$j]) {
+        $tmp = $array[$j + 1];
+        $array[$j + 1] = $array[$j];
+        $array[$j] = $tmp;
+    }
 }
+}
+
 // 修正はここまで
 
 echo "<pre>";
