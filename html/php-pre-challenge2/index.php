f09fb49e28abc0c428cba407773c2c793e146f12
hayato-nima <kdc98bac72xkx@gmail.com>
2020-10-19 16:07:35 +0900

    update:コメントとコード間に空白を入れました

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,14 +4,14 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 
 $count = count($array);
-for ($i = 0; $i < $count; $i++) {//大きな値を確定させていくループ
-  for ($j = 0; $j < ($count - 1 - $i); $j++) {//上のループで確定させた数字を除いてループ
-    if ($array[$j + 1] < $array[$j]) {//隣り合う要素との大小判定
-        $tmp = $array[$j + 1];//$tmp 値の交換用の入れ物
-        $array[$j + 1] = $array[$j];
-        $array[$j] = $tmp;
+for ($i = 0; $i < $count; $i++) { //大きな値を確定させていくループ
+  for ($j = 0; $j < ($count - 1 - $i); $j++) { //上のループで確定させた数字を除いてループ
+    if ($array[$j + 1] < $array[$j]) { //隣り合う要素との大小判定
+      $tmp = $array[$j + 1]; //$tmp 値の交換用の入れ物
+      $array[$j + 1] = $array[$j];
+      $array[$j] = $tmp;
     }
- }
+  }
 }
 
 // 修正はここまで
